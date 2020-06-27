20a7ee946d8a39c60c7155614faf79f1e320d3d9
renu.bansal <renu.bansal@thoughtworks.com>
2020-06-27 10:26:00 +0530

    Refactor frequent point variable

--- src/main/java/com/thoughtworks/movierental/Rental.java
+++ src/main/java/com/thoughtworks/movierental/Rental.java
@@ -2,8 +2,8 @@ package com.thoughtworks.movierental;
 
 public class Rental {
 
-    public static final int BLURAY_BONUS_FREQUENT_POINTS = 4;
-    private static final int BONUS_FREQUENT_RENTER_POINTS = 2;
+    public static final int BLURAY_FREQUENT_RENTER_POINTS = 4;
+    private static final int NEW_RELEASE_FREQUENT_RENTER_POINTS = 2;
     private static final int DEFAULT_FREQUENT_RENTER_POINTS = 1;
     private Movie movie;
     private int daysRented;
@@ -21,8 +21,8 @@ public class Rental {
         if (!isBonusApplicable())
             return DEFAULT_FREQUENT_RENTER_POINTS;
         if (movie.isNewRelease())
-            return BONUS_FREQUENT_RENTER_POINTS;
-        return BLURAY_BONUS_FREQUENT_POINTS;
+            return NEW_RELEASE_FREQUENT_RENTER_POINTS;
+        return BLURAY_FREQUENT_RENTER_POINTS;
     }
 
     double amount() {
